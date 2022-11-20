a622b58e7e37d0a1893f57eca980be36b96dad4b
Roba Nath Basnet <robanbth@gmail.com>
2022-11-21 02:53:37 +0600

    cart modal added by portal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Cart from "./components/Cart/Cart";
 import Meals from "./components/Meals/Meals";
 function App() {
 	return (
 		<Fragment>
+			<Cart />
 			<Header />
 			<main>
 				<Meals />

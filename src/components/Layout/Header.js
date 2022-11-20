c76b9759cacbd8a63edbcfb453961fd554b492db
Roba Nath Basnet <robanbth@gmail.com>
2022-11-20 13:53:44 +0600

    Header component added with styles

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCardButton";
+const Header = (props) => {
+	return (
+		<Fragment>
+			<header className={classes.header}>
+				<h1>McDolalds</h1>
+				<HeaderCardButton />
+			</header>
+			<div className={classes["main-image"]}>
+				<img src={mealsImage} alt="A table full of delicious meals!" />
+			</div>
+		</Fragment>
+	);
+};
+
+export default Header;

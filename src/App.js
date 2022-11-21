bcd1d57140f23a1b9a7f41ec52cd08fa95928ffd
Roba Nath Basnet <robanbth@gmail.com>
2022-11-21 16:53:52 +0600

    cartContext added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Cart from "./components/Cart/Cart";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 function App() {
 	const [cartIsShown, setCartIsShown] = useState("");
 
@@ -14,13 +15,13 @@ function App() {
 	};
 
 	return (
-		<Fragment>
+		<CartProvider>
 			{cartIsShown && <Cart onClose={hideCartHandler} />}
 			<Header onShowcart={showCartHandler} />
 			<main>
 				<Meals />
 			</main>
-		</Fragment>
+		</CartProvider>
 	);
 }
 
---
+++ src/store/CartProvider.js
@@ -0,0 +1,19 @@
+import { Children } from "react";
+import CartContext from "./cart-context";
+const CartProvider = (props) => {
+	const addItemToCartHandler = (item) => {};
+	const removeItemFromCartHandler = (id) => {};
+	const CartContext = {
+		items: [],
+		totalAmount: 0,
+		addItem: addItemToCartHandler,
+		removeItem: removeItemFromCartHandler,
+	};
+	return (
+		<CartContext.Provider value={CartContext}>
+			{props.Children}
+		</CartContext.Provider>
+	);
+};
+
+export default CartProvider;
